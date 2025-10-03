c7868742ab012bb3f8a00e62938ac467022ed5b0
YogzV <yoganandamv04@gmail.com>
2025-10-03 13:55:42 +0530

    Time: 22 ms (28.35%) | Memory: 46 MB (96.68%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.java
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -19,41 +19,39 @@ class Solution {
         int i;
         int maxWater = 0;
 
-        boolean[][] vis = new boolean[row][col];
-        for(i=0;i<row;i++)
-        {
-            Arrays.fill(vis[i],false);
-        }
+        int[][] vis = new int[row][col];
+    
         
         for(i=0;i<row;i++)
         {
             pqueue.offer(new cell(heightMap[i][0],i,0));
             pqueue.offer(new cell(heightMap[i][col-1],i,col-1));
-            vis[i][0] = vis[i][col-1] = true;
+            vis[i][0] = vis[i][col-1] = 1;
         }
 
         for(i=1;i<col-1;i++)
         {
             pqueue.offer(new cell(heightMap[0][i],0,i));
             pqueue.offer(new cell(heightMap[row-1][i],row-1,i));
-            vis[0][i] = vis[row-1][i] = true;
+            vis[0][i] = vis[row-1][i] = 1;
         }
 
         int dx[] = {-1,0,1,0};
         int dy[] = {0,1,0,-1};
+        int x,y;
 
         while(!pqueue.isEmpty())
         {
            cell obj = pqueue.poll();
            for(i=0;i<4;i++)
            {
-            int x = dx[i] + obj.r;
-            int y = dy[i] + obj.c;
-            if(x<row && x>0 && y<col && y>0 && !vis[x][y])
+             x = dx[i] + obj.r;
+             y = dy[i] + obj.c;
+            if(x<row && x>0 && y<col && y>0 && vis[x][y]==0)
             {
                 maxWater += Math.max(0,obj.height-heightMap[x][y]);
                 pqueue.offer(new cell(Math.max(obj.height,heightMap[x][y]),x,y));
-                vis[x][y] = true;
+                vis[x][y] =1;
             } 
 
            }
