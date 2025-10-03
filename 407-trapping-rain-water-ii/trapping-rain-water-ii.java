e38ea8a8ba10d5cc2480567659043bc3a5878024
YogzV <yoganandamv04@gmail.com>
2025-10-03 14:26:20 +0530

    Time: 20 ms (51.73%) | Memory: 46.7 MB (24.76%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.java
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -1,41 +1,52 @@
 class Solution {
 
-    private static class cell{
+    public static class Cell implements Comparable<Cell>{
     int height;
     int r;
     int c;
-    cell(int h,int r,int c)
+    Cell(int h,int r,int c)
     {
         height = h;
         this.r = r;
         this.c = c;
     }
+
+    @Override
+    public int compareTo(Cell obj){
+        return Integer.compare(this.height,obj.height);
+    }
 }
 
 
     public int trapRainWater(int[][] heightMap) {
+
+if (heightMap == null || heightMap.length <= 2 || heightMap[0].length <= 2) return 0;
+
         int row = heightMap.length;
         int col = heightMap[0].length;
-        PriorityQueue<cell> pqueue = new PriorityQueue<>(Comparator.comparingInt(obj -> obj.height));
+        PriorityQueue<Cell> pqueue = new PriorityQueue<>();
+         int i;
+         int maxWater = 0;
 
-        int i;
-        int maxWater = 0;
-
-        int[][] vis = new int[row][col];
+        boolean[][] vis = new boolean[row][col];
+        for(i=0;i<row;i++)
+        {
+            Arrays.fill(vis[i],false);
+        }
     
         
         for(i=0;i<row;i++)
         {
-            pqueue.offer(new cell(heightMap[i][0],i,0));
-            pqueue.offer(new cell(heightMap[i][col-1],i,col-1));
-            vis[i][0] = vis[i][col-1] = 1;
+            pqueue.offer(new Cell(heightMap[i][0],i,0));
+            pqueue.offer(new Cell(heightMap[i][col-1],i,col-1));
+            vis[i][0] = vis[i][col-1] = true;
         }
 
         for(i=1;i<col-1;i++)
         {
-            pqueue.offer(new cell(heightMap[0][i],0,i));
-            pqueue.offer(new cell(heightMap[row-1][i],row-1,i));
-            vis[0][i] = vis[row-1][i] = 1;
+            pqueue.offer(new Cell(heightMap[0][i],0,i));
+            pqueue.offer(new Cell(heightMap[row-1][i],row-1,i));
+            vis[0][i] = vis[row-1][i] = true;
         }
 
         int dx[] = {-1,0,1,0};
@@ -44,26 +55,22 @@ class Solution {
 
         while(!pqueue.isEmpty())
         {
-           cell obj = pqueue.poll();
+           Cell obj = pqueue.poll();
            for(i=0;i<4;i++)
            {
              x = dx[i] + obj.r;
              y = dy[i] + obj.c;
-            if(x>=row || x<0 || y>=col || y<0 || vis[x][y]==1)
+            if(x<row && x>0 && y<col && y>0 && !vis[x][y])
             {
-                continue;
-            } else{
+            
                 maxWater += Math.max(0,obj.height-heightMap[x][y]);
-                pqueue.offer(new cell(Math.max(obj.height,heightMap[x][y]),x,y));
-                vis[x][y] =1;
+                pqueue.offer(new Cell(Math.max(obj.height,heightMap[x][y]),x,y));
+                vis[x][y] =true;
             }
 
            }
 
         }
-
-
-
         return maxWater;
         
     }
