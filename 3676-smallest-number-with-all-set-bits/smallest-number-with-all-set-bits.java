f418c2a90e1846a98fce1cd82da463eb29a0562e
YogzV <yoganandamv04@gmail.com>
2025-10-12 10:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (58.77%) - LeetSync

---
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int smallestNumber(int n) {
+        int i=0;
+        while(true)
+        {
+            int val = (int)Math.pow(2,i);
+            if(val > n)
+            {
+                break;
+            }
+            i++;
+        }
+        
+        i--;
+        
+        
+        
+        if((int)Math.pow(2,i) -1  == n) return n;
+        return ((int)Math.pow(2,i+1))-1;
+    }
+}
