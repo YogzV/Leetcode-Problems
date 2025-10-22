b7ca154ac75946c7a105277ecdbd129c30566775
YogzV <yoganandamv04@gmail.com>
2025-10-22 22:30:29 +0530

    Time: 150 ms (55.56%) | Memory: 58.3 MB (87.50%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -1,27 +1,57 @@
+import java.util.*;
+
 class Solution {
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        Map<Integer, Integer> pointsCover = new HashMap<>();
-        Map<Integer, Integer> cntPoints = new HashMap<>();
-        Set<Integer> points = new TreeSet<>(); // TreeSet to keep points sorted
+        int n = nums.length;
+        if (n == 0) return 0;
+        Arrays.sort(nums);
 
-        for (int num : nums) {
-            cntPoints.put(num, cntPoints.getOrDefault(num, 0) + 1);
-            pointsCover.put(num - k, pointsCover.getOrDefault(num - k, 0) + 1);
-            pointsCover.put(num + k + 1, pointsCover.getOrDefault(num + k + 1, 0) - 1);
-            points.add(num);
-            points.add(num - k);
-            points.add(num + k + 1);
+        Map<Long, Integer> freq = new HashMap<>();
+        for (int x : nums) freq.put((long)x, freq.getOrDefault((long)x, 0) + 1);
+
+        int ans = 1;
+
+        for (Map.Entry<Long, Integer> entry : freq.entrySet()) {
+            long v = entry.getKey();
+            int already = entry.getValue();
+
+            long lowVal = v - k;
+            long highVal = v + k;
+            int L = lowerBound(nums, lowVal);
+            int R = upperBound(nums, highVal);
+            int totalInRange = R - L;
+            int need = totalInRange - already;
+            int canFix = Math.min(need, numOperations);
+            ans = Math.max(ans, already + canFix);
         }
 
-        int res = 0;
-        int pointsCoverThisPoint = 0;
-
-        for (int point : points) {
-            pointsCoverThisPoint += pointsCover.getOrDefault(point, 0);
-            res = Math.max(res, cntPoints.getOrDefault(point, 0) + 
-                        Math.min(pointsCoverThisPoint - cntPoints.getOrDefault(point, 0), numOperations));
+        int l = 0;
+        for (int r = 0; r < n; ++r) {
+            while (l <= r && (long)nums[r] - nums[l] > 2L * k) l++;
+            int w = r - l + 1;
+            ans = Math.max(ans, Math.min(w, numOperations));
         }
 
-        return res;
+        return ans;
+    }
+
+    private int lowerBound(int[] arr, long target) {
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            if ((long)arr[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+
+    private int upperBound(int[] arr, long target) {
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            if ((long)arr[mid] <= target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
     }
 }
