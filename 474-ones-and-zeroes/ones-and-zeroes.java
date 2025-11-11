1568ab143e0bdd9d47dc953b7b4dad2f93d93007
YogzV <yoganandamv04@gmail.com>
2025-11-11 20:59:09 +0530

    Time: 46 ms (43.76%) | Memory: 74.1 MB (36.67%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -2,50 +2,36 @@ class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
         int i,j;
         int size = strs.length;
-        int[] ones = new int[size];
-        int[] zeros = new int[size];
+        int[][] freq = new int[size][2];
         int[][][] dp = new int[m+1][n+1][size];
-        for(i=0;i<m+1;i++)
-        {
-            for(j=0;j<n+1;j++){
-                Arrays.fill(dp[i][j],-1);
-                
-            }
-        }
-        for(i=0;i<size;i++)
-        {
-            int len = strs[i].length();
-            for(j=0;j<len;j++)
-            {
-                if(strs[i].charAt(j) == '1')
-                 ones[i]++;
-                else
-                 zeros[i]++;
-            }
-        }
         
-
-        return findMax(ones,zeros,m,n,0,dp);
+        for(i=0;i<m+1;i++)
+            for(j=0;j<n+1;j++)
+                Arrays.fill(dp[i][j],-1);   
+            
+        for(i=0;i<size;i++)
+            for(j=strs[i].length()-1;j>=0;j--)
+                freq[i][strs[i].charAt(j) == '1'? 1 : 0]++;
+            
+        return findMax(freq,m,n,0,dp);
      }
 
-     public static int findMax(int[] ones,int[] zeros,int maxZero,int maxOne,int ind,int[][][] dp){
+     public static int findMax(int[][] freq,int maxZero,int maxOne,int ind,int[][][] dp){
    
-       if(ind == ones.length)
+       if(ind == freq.length)
         return 0;
        if(dp[maxZero][maxOne][ind] != -1)
         return dp[maxZero][maxOne][ind];
 
-       int notTake = findMax(ones,zeros,maxZero,maxOne,ind+1,dp);
+       int notTake = findMax(freq,maxZero,maxOne,ind+1,dp);
 
        int take = 0;
-       int zero = zeros[ind];
-       int one = ones[ind];
-       if(one<= maxOne && zero<= maxZero)
-       {
-        take = 1 + findMax(ones,zeros,maxZero-zero,maxOne-one,ind+1,dp);
-       }
-      
+       int zero = freq[ind][0];
+       int one = freq[ind][1];
 
+       if(one<= maxOne && zero<= maxZero)
+         take = 1 + findMax(freq,maxZero-zero,maxOne-one,ind+1,dp);
+       
        return dp[maxZero][maxOne][ind] = Math.max(take,notTake);
             
      }
