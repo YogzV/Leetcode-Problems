5a1edb2d8557b863f076bbbcc6724a2d62c4ab19
YogzV <yoganandamv04@gmail.com>
2025-12-06 19:26:52 +0530

    Time: 9 ms (22.63%) | Memory: 46.6 MB (18.12%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,59 +1,28 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int i,j;
-        for(i=0;i<9;i++)
+        HashMap<Integer,Set<Character>> row = new HashMap<>();
+        HashMap<Integer,Set<Character>> col = new HashMap<>();
+        HashMap<String,Set<Character>> grid = new HashMap<>();
+        for(int i=0;i<9;i++)
         {
-            Set<Character> set = new HashSet<>();
-       
-            for(j=0;j<9;j++)
+            for(int j = 0;j<9;j++)
             {
-              if(board[i][j] != '.'){
-               if(set.contains(board[i][j]))
-                return false;
-               set.add(board[i][j]);
-              }
-                
+                if(board[i][j] == '.') continue;
+
+                String gridval = (i/3) + "#" + (j/3);
+
+                if(row.computeIfAbsent(i,k -> new HashSet<>()).contains(board[i][j]) 
+                   || col.computeIfAbsent(j,k -> new HashSet<>()).contains(board[i][j]) || grid.
+                    computeIfAbsent(gridval,k -> new HashSet<>()).contains(board[i][j]))
+                 return false;
+
+                row.get(i).add(board[i][j]);
+                col.get(j).add(board[i][j]);
+                grid.get(gridval).add(board[i][j]);
             }
-            
+
+           
         }
-       
-
-        for(i=0;i<9;i++)
-        {
-            Set<Character> set = new HashSet<>();
-            for(j=0;j<9;j++)
-            {
-                if(board[j][i] != '.'){
-               if(set.contains(board[j][i]))
-                return false;
-               set.add(board[j][i]);
-                }
-            }
-        }
-        
-
-        for(i=0;i<9;i+=3)
-        {
-            for(j=0;j<9;j+=3)
-            {
-                Set<Character> set = new HashSet<>();
-                for(int k = i;k<i+3;k++)
-                {
-                    for(int l = j;l<j+3;l++)
-                    {
-                        if(board[k][l] != '.'){
-                      if(set.contains(board[k][l]))
-                       return false;
-                      set.add(board[k][l]);
-                        }
-                    }
-                }
+         return true;
     }
-    }
-            
-        
-        return true;
-    
-    }
-
 }
