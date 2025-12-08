7edc0e8355d3d59ec994ee93e30cc6f1bdba8ed1
YogzV <yoganandamv04@gmail.com>
2025-12-08 19:54:24 +0530

    Time: 82 ms (66.66%) | Memory: 96.2 MB (38.86%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -6,19 +6,19 @@ class Solution {
         int[][] pair = new int[len][2];
         for(i=0;i<len;i++)
         {
-            pair[i][0] = position[i];
+            pair[i][0] = target - position[i];
             pair[i][1] = speed[i]; 
         }
-        Arrays.sort(pair,(a,b) -> b[0] - a[0]);
+        Arrays.sort(pair,(a,b) ->a[0] - b[0]);
         Stack<Double> stack = new Stack<>();
-        int ans = 0;
+        
         for(i=0;i<len;i++)
         {
-           double val = (target - pair[i][0])/(double)pair[i][1];
+           double val = pair[i][0]/(double)pair[i][1];
            
            if(stack.isEmpty() || stack.peek() < val ){
                  stack.push(val);
-                 ans+=1;             
+                              
            }
         }
         return stack.size();
