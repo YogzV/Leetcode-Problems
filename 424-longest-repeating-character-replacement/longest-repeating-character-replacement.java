fa45f73a8c2ef191854fc13cd5f266287750ee4d
YogzV <yoganandamv04@gmail.com>
2025-12-11 16:23:01 +0530

    Time: 8 ms (72.49%) | Memory: 46.1 MB (40.93%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -6,20 +6,20 @@ class Solution {
         int maxLen = 0;
         int maxFreq = 0;
         int len = s.length();
+        int changes;
         while(sp < len)
         {
             int val = s.charAt(sp) - 65;
             freq[val]++;
             maxFreq = Math.max(maxFreq,freq[val]);
-
-            if((sp-fp+1 - maxFreq) <= k){
+            changes = sp-fp+1 - maxFreq;
+            if(changes <= k){
               maxLen = Math.max(maxLen, sp-fp+1);
             }
             else{
-                while(sp-fp+1-maxFreq > k ){
                  freq[s.charAt(fp) - 65]--;
                  fp++;
-                }
+                
             }
             sp++;
 
