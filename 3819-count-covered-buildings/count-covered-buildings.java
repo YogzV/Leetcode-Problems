b03890d147afcca3dc0ed35d02d0a7728ebdba72
YogzV <yoganandamv04@gmail.com>
2025-12-11 12:53:03 +0530

    Time: 27 ms (72.63%) | Memory: 221.8 MB (57.90%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.java
+++ 3819-count-covered-buildings/count-covered-buildings.java
@@ -3,48 +3,38 @@ class Solution {
 
         int[][] row = new int[2][n];
         int[][] col = new int[n][2];
-        int i,j;
+       
         int x,y;
-        int min,max;
+      
         int ans = 0;
 
 
          Arrays.fill(row[1],-1);
-         Arrays.fill(row[0],-1);
+         Arrays.fill(row[0],n);
 
-       for(i=0;i<n;i++)
+       for(int i=0;i<n;i++)
        {
-       
         Arrays.fill(col[i],-1);
        }   
 
-        for(i = buildings.length-1;i>=0;i--)
+        for(int i = buildings.length-1;i>=0;i--)
         {
             x = buildings[i][0] - 1;
             y = buildings[i][1] - 1 ;
             
-            min = row[0][y] == -1 ?  x : Math.min(row[0][y],x);
-            max = row[1][y] == -1 ? x: Math.max(row[1][y],x);
-            row[0][y] = min;
-            row[1][y] = max;
+            row[0][y] = Math.min(row[0][y],x);
+            row[1][y] = Math.max(row[1][y],x);
+            
 
-            min = col[x][0] ==-1? y : Math.min(col[x][0],y);
-            max = col[x][1] == -1? y : Math.max(col[x][1],y);
-            col[x][0] = min;
-            col[x][1] = max;
+            col[x][0] = ( col[x][0] ==-1? y : Math.min(col[x][0],y) );
+            col[x][1] = ( col[x][1] == -1? y : Math.max(col[x][1],y) );
+            
         }
        
-        // for(i=0;i<n;i++)
-        // {
-        //     for(j=0;j<n;j++)
-        //     {
-        //         System.out.print(city[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
+     
         
        
-        for(i = buildings.length-1;i>=0;i--)
+        for(int i = buildings.length-1;i>=0;i--)
         {
              x = buildings[i][0] - 1;
              y = buildings[i][1] - 1;
