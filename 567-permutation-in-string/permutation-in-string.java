11d616d811c07bb1c7fed79e2cb812b7093d8647
YogzV <yoganandamv04@gmail.com>
2025-12-19 17:07:01 +0530

    Time: 7 ms (67.91%) | Memory: 44.3 MB (37.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,42 +2,33 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
-        int i;
-        
-        int len = s1.length();
+        int fp=0;
+        int val1,val2;
+        int sp = s1.length(); 
         int len2 = s2.length();
-        if(len > len2) return false;
-        for(i=0;i<len;i++)
+
+        if(sp > len2) return false;
+
+        for(int i=0;i<sp;i++)
         {
             freq1[s1.charAt(i) - 'a']++;
             freq2[s2.charAt(i) - 'a']++;
         }
-        int match = 0;
-        for(i=0;i<26;i++)
-        {
-          if(freq1[i] == freq2[i]) 
-            match++;
-        }
+
+        if(matches(freq1,freq2))
+            return true;
        
-         int fp=0;
-         int sp = len;
-         
-         
          while(sp < len2){
-            if(match == 26) return true;
-            int val1 = s2.charAt(fp) - 'a';
-            int val2 = s2.charAt(sp) - 'a';
-            freq2[val1]--;
-            fp++;
-            freq2[val2]++;  
+            
+            freq2[s2.charAt(fp++) - 'a']--;
+            freq2[s2.charAt(sp++) - 'a']++;  
 
            if(matches(freq1,freq2))
             return true;
-
-            sp++;
+            
          }
-         return matches(freq1,freq2);
 
+         return false;
     }
 
     public static boolean matches(int[] freq1,int[] freq2){
