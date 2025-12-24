dfdff60a024b780c2e06207b032b8d509d4230ee
YogzV <yoganandamv04@gmail.com>
2025-12-24 19:49:29 +0530

    Time: 46 ms (63.02%) | Memory: 133.5 MB (15.87%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,25 +1,78 @@
+class Node{
+    int key;
+    int val;
+    Node prev;
+    Node next;
+    Node(int key,int val)
+    {
+        this.key = key;
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
 class LRUCache {
 
-     int capacity;
-     Map<Integer,Integer> cache;
+    int capacity;
+    HashMap<Integer,Node> hmap;
+    Node left;
+    Node right;
+
 
     public LRUCache(int capacity) {
-          this.capacity = capacity;
-          this.cache = new LinkedHashMap<>(capacity,1f,true){
-            protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest){
-                return size() > LRUCache.this.capacity;
-            };
-          };  
+        this.capacity = capacity;
+        hmap = new HashMap<>();
+        this.left = new Node(0,0);
+        this.right = new Node(0,0);
+        this.left.next = this.right;
+        this.right.prev = this.left;
+
+    }
+
+    public void remove(Node node)
+    {
+        Node previous = node.prev;
+        Node after = node.next;
+        previous.next = after;
+        after.prev = previous;
+    }
+
+    public void insert(Node node)
+    {
+        Node previous = this.right.prev;
+        previous.next = node;
+        node.prev = previous;
+        node.next = this.right;
+        this.right.prev = node;
     }
     
     public int get(int key) {
-        return cache.getOrDefault(key,-1);
+        if(!hmap.containsKey(key))
+          return -1;
         
+        Node node = hmap.get(key);
+        remove(node);
+        insert(node);
+        return node.val;
     }
     
     public void put(int key, int value) {
-       cache.put(key,value);
-       return;
+        if(hmap.containsKey(key))
+        {
+            remove(hmap.get(key));
+        }
+        Node node = new Node(key,value);
+        insert(node);
+        hmap.put(key,node);
+
+        if(hmap.size() > capacity)
+        {
+            hmap.remove(this.left.next.key);
+            remove(this.left.next);
+        
+        }
+
     }
 }
 
