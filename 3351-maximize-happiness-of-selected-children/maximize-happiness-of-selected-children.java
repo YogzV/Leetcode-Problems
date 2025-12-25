7d9441302e8b25c8924455226f5e53db515e9d93
YogzV <yoganandamv04@gmail.com>
2025-12-25 17:02:21 +0530

    Time: 43 ms (43.55%) | Memory: 109.3 MB (34.95%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -1,16 +1,13 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
-        Queue<Integer> pqueue = new PriorityQueue<>(Collections.reverseOrder());
-        for(int num : happiness){
-            pqueue.offer(num);
-        }
-        long dec = 0;
+        Arrays.sort(happiness);
         long ans = 0;
-        while(k-- > 0)
-        {
-           ans += Math.max(0,(pqueue.poll() - dec ));
-           dec++;
+        int ind = happiness.length - 1;
+        for(int i=0;i<k;i++){
+            ans += Math.max(0,happiness[ind] - i);
+            ind--;
         }
         return ans;
+        
     }
 }
