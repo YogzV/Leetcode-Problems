249727a3faf755f7f7755ed2fb7ae5d27ea33c2a
YogzV <yoganandamv04@gmail.com>
2025-12-26 13:18:04 +0530

    Time: 6 ms (91.43%) | Memory: 47.7 MB (46.27%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,44 +9,12 @@
  */
 
 class Solution {
-    TreeNode ances;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(singleFind(p.left,q) || singleFind(p.right,q)) 
-          return p;
-        
-        if(singleFind(q.left,p) || singleFind(q.right,p))
-          return q;
- 
-        find(root,p,q);
-        return this.ances;
-        
-    }
-
-    public boolean singleFind(TreeNode root,TreeNode find)
-    {
-        if(root == null) return false;
-        if(root == find) return true;
-
-        return singleFind(root.left,find) || singleFind(root.right,find);
-    }
-
-    public boolean find(TreeNode root,TreeNode p,TreeNode q){
-        if(root == null) return false;
-
-         if(root == p || root==q)
-         {
-            return true;
-         }
-
-         boolean left = find(root.left,p,q);
-         boolean right = find(root.right,p,q);
-
-         if(left == true && right==true){
-            this.ances = root;
-            return true;
-         }else if(left || right){
-            return true;
-         }
-         return false;
+        if(Math.max(p.val,q.val) < root.val)
+         return lowestCommonAncestor(root.left,p,q);
+        else if(Math.min(p.val,q.val) > root.val)
+         return lowestCommonAncestor(root.right,p,q);
+        else 
+         return root;
     }
 }
