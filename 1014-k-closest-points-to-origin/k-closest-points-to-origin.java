530e42abc8a67fff6245a5df548f040567c4718f
YogzV <yoganandamv04@gmail.com>
2026-01-07 19:43:34 +0530

    Time: 39 ms (22.32%) | Memory: 59.4 MB (13.34%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,42 @@
+class Pair implements Comparable<Pair>{
+    int[] pt;
+    double dist;
+    Pair(int[] pt ,double dist){
+        this.pt = pt;
+        this.dist = dist;
+    }
+
+    @Override
+    public int compareTo(Pair other){
+        return Double.compare(other.dist,this.dist);
+    }
+}
+
+class Solution {
+
+    public int[][] kClosest(int[][] points, int k) {
+        Queue<Pair> pqueue = new PriorityQueue<>();
+        int[][] ans = new int[k][2];
+
+        for(int[] pt : points){
+            double dist = calculateDistance(pt[0],pt[1]);
+            pqueue.offer(new Pair(pt,dist));
+            if(pqueue.size() > k){
+                pqueue.poll();
+            } 
+        }
+        
+        while(k-- > 0){
+          int[] pt = pqueue.poll().pt;
+          ans[k][0] = pt[0];
+          ans[k][1] = pt[1];
+        }
+
+        return ans;
+    }
+
+    public double calculateDistance(int x1,int y1){
+        int val = x1 * x1 + y1 * y1;
+        return Math.sqrt(val);
+    }
+}
