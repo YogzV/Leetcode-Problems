975c635b4593356fbca15fd9a53b5546703f1cd0
YogzV <yoganandamv04@gmail.com>
2026-01-09 00:08:40 +0530

    Time: 138 ms (58.09%) | Memory: 111.6 MB (27.26%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,67 +1,34 @@
-class MedianFinder {
+public class MedianFinder {
 
-    Queue<Integer> lMaxHeap;
-    Queue<Integer> rMinHeap;
+    private Queue<Integer> smallHeap; //small elements - maxHeap
+    private Queue<Integer> largeHeap; //large elements - minHeap
 
     public MedianFinder() {
-        lMaxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        rMinHeap = new PriorityQueue<>();
+        smallHeap = new PriorityQueue<>((a, b) -> b - a);
+        largeHeap = new PriorityQueue<>((a, b) -> a - b);
     }
-    
+
     public void addNum(int num) {
-        if(rMinHeap.isEmpty()){
-            rMinHeap.offer(num);
-        }else if(lMaxHeap.isEmpty()){
-            lMaxHeap.offer(num);
-        }else{
-
-            if(lMaxHeap.peek() >= num && rMinHeap.peek() > num){
-                if(lMaxHeap.size() == rMinHeap.size()){
-                    rMinHeap.offer(lMaxHeap.poll());
-                   
-                }
-                 lMaxHeap.offer(num);
-
-            }else{
-                 if(rMinHeap.size()-1 == lMaxHeap.size()){
-                    if(rMinHeap.peek() > num){
-                      lMaxHeap.offer(num);
-                    }else{
-                        lMaxHeap.offer(rMinHeap.poll());
-                        rMinHeap.offer(num);
-                    }
-                    
-                 }else{
-                    rMinHeap.offer(num);
-                 }
-                 
-            }
-            
+        smallHeap.add(num);
+        if (
+            smallHeap.size() - largeHeap.size() > 1 ||
+            !largeHeap.isEmpty() &&
+            smallHeap.peek() > largeHeap.peek()
+        ) {
+            largeHeap.add(smallHeap.poll());
         }
-
-        if(lMaxHeap.size() == 1 && rMinHeap.size() ==1 && lMaxHeap.peek() > rMinHeap.peek()){
-            int temp = lMaxHeap.poll();
-            lMaxHeap.offer(rMinHeap.poll());
-            rMinHeap.poll();
-            rMinHeap.offer(temp);
+        if (largeHeap.size() - smallHeap.size() > 1) {
+            smallHeap.add(largeHeap.poll());
         }
-
     }
-    
-    public double findMedian() {
-        int size = lMaxHeap.size() + rMinHeap.size();
-        if((size&1) == 1){
-            return rMinHeap.peek();
-        }
 
-        return (rMinHeap.peek() + lMaxHeap.peek()) / 2.0;
-        
+    public double findMedian() {
+        if (smallHeap.size() == largeHeap.size()) {
+            return (double) (largeHeap.peek() + smallHeap.peek()) / 2;
+        } else if (smallHeap.size() > largeHeap.size()) {
+            return (double) smallHeap.peek();
+        } else {
+            return (double) largeHeap.peek();
+        }
     }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
