a63219f4f2afd75b8bb96a53e918f1b6e02ed194
YogzV <yoganandamv04@gmail.com>
2026-01-09 21:00:46 +0530

    Time: 2 ms (39.45%) | Memory: 45.6 MB (48.55%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+         
+         if(nums.length == 1){
+            return Arrays.asList(new ArrayList<>(Arrays.asList(nums[0])));
+         }
+
+        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> perms = permute(Arrays.copyOfRange(nums,1,nums.length));
+
+        for(List<Integer> perm : perms){
+            for(int i=0;i<=perm.size();i++){
+                List<Integer> copy = new ArrayList<>(perm);
+                copy.add(i,nums[0]);
+                res.add(copy);
+            }
+        }
+        return res;
+    }
+}
