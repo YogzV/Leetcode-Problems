0f8e61401226842ed12cf0cf3c8cc0b704065ced
YogzV <yoganandamv04@gmail.com>
2026-01-14 21:25:06 +0530

    Time: 186 ms (21.40%) | Memory: 48.2 MB (60.48%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,63 +1,28 @@
-class Pair{
-    String str;
-    int seq;
-    Pair(String str,int seq){
-        this.str = str;
-        this.seq = seq;
-    }
-}
-
-class Solution {
+public class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> set = new HashSet<>();
-        Set<String> vis = new HashSet<>();
-        int len = beginWord.length();
-        int ans = Integer.MAX_VALUE;
-        int i,j;
-        Pair pair;
-        int seq;
-        String str,tempStr;
-        StringBuilder temp;
+        if (!wordList.contains(endWord) || beginWord.equals(endWord)) return 0;
+        Set<String> words = new HashSet<>(wordList);
+        int res = 0;
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
 
-        for(String s : wordList){
-            set.add(s);
-        }
-
-        if(!set.contains(endWord))
-          return 0;
-
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(new Pair(beginWord,1));
-        vis.add(beginWord);
-
-        while(!queue.isEmpty()){
-            
-            pair = queue.poll();
-            str = pair.str;
-           
-            seq = pair.seq;
-            
-
-            for(i=0;i<len;i++){
-
-                temp = new StringBuilder(str);
-                
-                    for(j = 'a';j<='z';j++){
-
-                        temp.setCharAt(i,(char)j);
-                        tempStr = temp.toString();
-                        if(tempStr.equals(endWord)){
-                            ans = Math.min(ans,seq+1);
+        while (!q.isEmpty()) {
+            res++;
+            for (int i = q.size(); i > 0; i--) {
+                String node = q.poll();
+                if (node.equals(endWord)) return res;
+                for (int j = 0; j < node.length(); j++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == node.charAt(j)) continue;
+                        String nei = node.substring(0, j) + c + node.substring(j + 1);
+                        if (words.contains(nei)) {
+                            q.offer(nei);
+                            words.remove(nei);
                         }
-                        if(!vis.contains(tempStr) && set.contains(temp.toString())){
-                            vis.add(tempStr);
-                            queue.offer(new Pair(temp.toString(),seq+1));
-                        }
-                        
                     }
+                }
             }
         }
-
-        return ans==Integer.MAX_VALUE? 0 : ans;
+        return 0;
     }
 }
