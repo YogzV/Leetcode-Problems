637bfb817255d527475aa6eaaf88c5e1a7f78f13
YogzV <yoganandamv04@gmail.com>
2026-01-14 21:39:48 +0530

    Time: 19 ms (98.86%) | Memory: 47.7 MB (84.97%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,44 +1,54 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
-        Set<String> set = new HashSet<>(wordList);
-        if (!set.contains(endWord)) return 0;
+        Set<String> dict = new HashSet<>(wordList);
+        if (!dict.contains(endWord)) return 0;
 
-        Queue<String> queue = new ArrayDeque<>();
-        queue.offer(beginWord);
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+
+        beginSet.add(beginWord);
+        endSet.add(endWord);
 
         int level = 1;
         int len = beginWord.length();
 
-        while (!queue.isEmpty()) {
-            int size = queue.size(); // FIXED
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
 
-            for (int k = 0; k < size; k++) {
-                String word = queue.poll();
+            // Always expand smaller side
+            if (beginSet.size() > endSet.size()) {
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
 
+            Set<String> nextLevel = new HashSet<>();
+
+            for (String word : beginSet) {
                 char[] arr = word.toCharArray();
 
                 for (int i = 0; i < len; i++) {
-                    char original = arr[i];
+                    char old = arr[i];
 
                     for (char c = 'a'; c <= 'z'; c++) {
-                        if (c == original) continue;
+                        if (c == old) continue;
 
                         arr[i] = c;
                         String next = new String(arr);
 
-                        if (next.equals(endWord)) {
+                        if (endSet.contains(next)) {
                             return level + 1;
                         }
 
-                        if (set.remove(next)) {
-                            
-                            queue.offer(next);
+                        if (dict.remove(next)) {
+                            nextLevel.add(next);
                         }
                     }
-                    arr[i] = original; // restore
+                    arr[i] = old;
                 }
             }
+
+            beginSet = nextLevel;
             level++;
         }
 
