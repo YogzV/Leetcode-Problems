1d7562f14e22bdae52f0d3880783e1521456f2e2
YogzV <yoganandamv04@gmail.com>
2026-01-17 14:40:39 +0530

    Time: 16 ms (66.06%) | Memory: 46.2 MB (86.82%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,16 @@
 class Solution {
-    HashMap<Integer,Integer> hmap = new HashMap<>();
     public int coinChange(int[] coins, int amount) {
-        int coin = recur(coins,amount);
-        return (coin >= 1e7)? -1 : coin;
-
-    }
-
-    public int recur(int[] coins,int amount){
-
-        if(hmap.containsKey(amount))
-         return hmap.get(amount);
-
-        if(amount == 0)
-         return 0;
-
-        int res = (int)1e7;
-
-        for(int coin : coins){
-            if(amount - coin >= 0){
-                res = Math.min(res,1 + recur(coins,amount-coin));
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int i=1;i<amount+1;i++){
+            for(int coin : coins){
+                if(coin <= i){
+                    dp[i] = Math.min(dp[i],1+dp[i-coin]);
+                }
             }
         }
-        hmap.put(amount,res);
-        return res;
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
