1219d2df7362eb6d560463e75e1b858a168b8537
YogzV <yoganandamv04@gmail.com>
2026-01-18 20:10:17 +0530

    Time: 43 ms (49.94%) | Memory: 46.1 MB (75.32%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,34 +1,20 @@
 class Solution {
-
-    int[][] memo;
-   
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        memo = new int[n][n];
+        int[] dp = new int[n];
+        int max = 0;
+        for(int i = n-1 ;i >=0 ;i--){
+            int curr = 1;
+            for(int j = i+1;j<n;j++){
+                if(nums[i] < nums[j]){
+                    curr = Math.max(curr,1+dp[j]);
+                }
+            }
 
-        for(int[] row : memo)
-         Arrays.fill(row,-1);
+            dp[i] = curr;
+            max = Math.max(dp[i],max);
+        }
 
-        return recur(nums,0,-1);
-       
-    }
-
-    public int recur(int[] nums,int ind,int j){
-        
-        
-        if(ind >= nums.length )
-         return 0;
-
-        if(j!=-1 && memo[ind][j] != -1)
-         return memo[ind][j];
-        
-        int ans1 = (j==-1 || nums[j] < nums[ind])? 1+recur(nums,ind+1,ind) : 0;
-        int ans2 = recur(nums,ind+1,j);
-       
-        int val = Math.max(ans1,ans2);
-        if(j!= -1)
-         memo[ind][j] = val;
-         
-        return val;
+        return max;
     }
 }
