6caf59e9601d4246a5fcb75ae3f727fba899fca1
YogzV <yoganandamv04@gmail.com>
2026-01-22 21:07:56 +0530

    Time: 19 ms (88.55%) | Memory: 54.2 MB (26.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,29 +1,26 @@
 class Solution {
-    int[][] memo;
+    
     public int longestCommonSubsequence(String text1, String text2) {
-        memo = new int[text1.length()][text2.length()];
-        for(int[] row : memo){
-            Arrays.fill(row,-1);
+       
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1+1][len2+1];
+
+        Arrays.fill(dp[len1],0);
+        for(int i=0;i<len1;i++){
+            dp[i][len2] = 0;
         }
-        return recur(text1,text2,0,0);
-    }
 
-    public int recur(String str1,String str2,int find,int sind){
-        if(find == str1.length() || sind == str2.length())
-         return 0;
+        for(int i = len1-1;i>=0;i--){
+            for(int j=len2-1;j>=0;j--){
+               if(text1.charAt(i) == text2.charAt(j)){
+                dp[i][j] = 1 + dp[i+1][j+1];
+               }else{
+                dp[i][j] = Math.max(dp[i+1][j],dp[i][j+1]);
+               }
+            }
+        }
 
-        if(memo[find][sind] != -1)
-         return memo[find][sind];
-        
-        
-       int ans = 0;
-       if(str1.charAt(find) == str2.charAt(sind)){
-        ans = 1 + recur(str1,str2,find+1,sind+1);
-       }else{
-         ans = Math.max(ans,recur(str1,str2,find+1,sind));
-         ans = Math.max(ans,recur(str1,str2,find,sind+1));
-       }
-
-       return memo[find][sind] = ans;
+        return dp[0][0];
     }
 }
